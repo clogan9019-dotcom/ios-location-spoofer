5977d6bc5d8784848808e8052905a74ba152b9ba
Bot man <clogan9019@gmail.com>
2026-05-21 14:37:46 -0400

    fix: remove auto-exploit on scene activation

--- LocationSpoofer/LocationSpooferApp.swift
+++ LocationSpoofer/LocationSpooferApp.swift
@@ -3,17 +3,11 @@ import SwiftUI
 @main
 struct LocationSpooferApp: App {
     @StateObject private var kernelManager = KernelLocationManager.shared
-    @Environment(\.scenePhase) private var scenePhase
 
     var body: some Scene {
         WindowGroup {
             ContentView()
                 .environmentObject(kernelManager)
-        }
-        .onChange(of: scenePhase) { phase in
-            if phase == .active {
-                kernelManager.restoreIfNeeded()
-            }
         }
     }
 }
