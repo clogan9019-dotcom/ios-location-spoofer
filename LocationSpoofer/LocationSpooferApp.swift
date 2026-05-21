794dccd5341c272151849da9d5772759e3a9c2a4
Bot man <clogan9019@gmail.com>
2026-05-21 16:31:44 -0400

    feat: upload log when app backgrounds

--- LocationSpoofer/LocationSpooferApp.swift
+++ LocationSpoofer/LocationSpooferApp.swift
@@ -3,11 +3,18 @@ import SwiftUI
 @main
 struct LocationSpooferApp: App {
     @StateObject private var kernelManager = KernelLocationManager.shared
+    @Environment(\.scenePhase) private var scenePhase
 
     var body: some Scene {
         WindowGroup {
             ContentView()
                 .environmentObject(kernelManager)
+        }
+        .onChange(of: scenePhase) { phase in
+            if phase == .background {
+                // Upload log whenever the app is sent to the background.
+                LogUploader.shared.uploadLog()
+            }
         }
     }
 }
