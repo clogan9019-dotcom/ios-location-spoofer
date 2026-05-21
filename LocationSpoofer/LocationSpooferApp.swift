2af1482f944aca755f065525843ed6f1618f1226
Bot man <clogan9019@gmail.com>
2026-05-21 12:20:16 -0400

    fix: watch scenePhase and call restoreIfNeeded on foreground

--- LocationSpoofer/LocationSpooferApp.swift
+++ LocationSpoofer/LocationSpooferApp.swift
@@ -3,11 +3,17 @@ import SwiftUI
 @main
 struct LocationSpooferApp: App {
     @StateObject private var kernelManager = KernelLocationManager.shared
+    @Environment(\.scenePhase) private var scenePhase
 
     var body: some Scene {
         WindowGroup {
             ContentView()
                 .environmentObject(kernelManager)
+        }
+        .onChange(of: scenePhase) { phase in
+            if phase == .active {
+                kernelManager.restoreIfNeeded()
+            }
         }
     }
 }
