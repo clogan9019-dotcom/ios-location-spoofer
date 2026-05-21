fa8fe68cb49b9ceb799d4862b603e8598d28374f
Bot man <clogan9019@gmail.com>
2026-05-21 10:02:08 -0400

    feat: add bridging header for kexploit C/ObjC files

---
+++ LocationSpoofer/LocationSpoofer-Bridging-Header.h
@@ -0,0 +1,27 @@
+//  LocationSpoofer-Bridging-Header.h
+
+#ifndef LocationSpoofer_Bridging_Header_h
+#define LocationSpoofer_Bridging_Header_h
+
+#include "kexploit/darksword.h"
+#include "kexploit/offsets.h"
+#include "kexploit/utils.h"
+#include "kexploit/persistence.h"
+#include "kexploit/compat.h"
+#include "kexploit/pe/vfs.h"
+#include "kexploit/pe/sbx.h"
+#include "kexploit/pe/vnode.h"
+#include "kexploit/pe/apfs.h"
+#include "kexploit/pe/rc.h"
+#include "kexploit/pe/xpaci.h"
+#include "kexploit/TaskRop/RemoteCall.h"
+#include "kexploit/TaskRop/vm.h"
+#include "kexploit/TaskRop/exc.h"
+#include "kexploit/TaskRop/pac.h"
+#include "kexploit/TaskRop/thread.h"
+#include "kexploit/TaskRop/privateapi.h"
+#include "headers/libgrabkernel2.h"
+#include "headers/xpf.h"
+#include "headers/fileport.h"
+
+#endif
